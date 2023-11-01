27dc27a73f563e98a41a88ab53656f198d1c9985
Mvoii <mvoifranklin@gmail.com>
2023-11-01 21:56:55 +0300

    initial commit

--- main.py
+++ main.py
@@ -2,5 +2,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route("/home")
+def home():
+    return("<p1>home</p>")
+@app.route("/login")
+def login():
+    return("<p>login page</p>")
+
 if __name__ == "__main__":
     app.run(debug=True)
